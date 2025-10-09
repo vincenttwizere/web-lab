c0c5f3cf9be689f57d57946b6a1f0d3185793b2c
Vincent TWIZERE <v.twizere@alustudent.com>
2025-10-09 21:32:43 +0200

    adding scripts

---
+++ script.js
@@ -0,0 +1,32 @@
+// learning javascript
+// variables are containers for storing data values
+
+let name = 'Eric';
+console.log(name);
+name = 'Peter';
+
+console.log('Hello ' + name);
+
+let firstName = 'olivin';
+let favoriteFood = 'Pizza';
+
+console.log('Hello ' + firstName + ', would you like to eat ' + favoriteFood + ' tonight?');
+console.log(`Hello ${firstName}, would you like to eat ${favoriteFood} tonight?`);
+
+let favoriteTeam, favoritePlayer;
+favoriteTeam = 'Chelsea';
+favoritePlayer = ' Cole Palmer';
+
+console.log(`my favorite team is ${favoriteTeam} and my favorite player is ${favoritePlayer}`);
+console.log(` I like it when ${favoritePlayer} scores a goal for ${favoriteTeam}`);
+
+console.log(`Hopefully ${favoriteTeam} will win the champions league this year! and ${favoritePlayer} will be the player of the tournament!`);
+
+let online = true;
+console.log(online);
+
+let forSale = true;
+console.log(`Is this car for sale? ${forSale}`);
+
+console.log(typeof forSale);
+console.log(`${typeof favoritePlayer}`);
