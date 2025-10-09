742de530e500a7be4ffc383ee2ded50b95643740
Vincent TWIZERE <v.twizere@alustudent.com>
2025-10-09 23:46:25 +0200

    latest updates

--- script.js
+++ script.js
@@ -30,3 +30,27 @@ console.log(`Is this car for sale? ${forSale}`);
 
 console.log(typeof forSale);
 console.log(`${typeof favoritePlayer}`);
+
+// modifying the content of a html element using javascript
+
+// document.getElementById('title').textContent = 'wagwan';
+// document.getElementById('title').style.color = 'red';
+
+// arthmetic operators
+
+ let numberOfStudents = 20;
+    numberOfStudents = numberOfStudents + 1;
+    numberOfStudents = numberOfStudents - 1;
+    numberOfStudents = numberOfStudents * 2;
+    numberOfStudents = numberOfStudents / 2;
+    numberOfStudents = numberOfStudents % 3;
+    numberOfStudents = numberOfStudents ** 3;
+
+ console.log(numberOfStudents);
+ console.log('hello ' + name);
+
+// accepting user input
+let userName = window.prompt('what is your name?');
+console.log(userName);
+
+let userAge = window.prompt('how old are you?');
