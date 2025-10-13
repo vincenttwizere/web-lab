208167ee8ef0c576fc2a3247334bf6bb871d1121
Vincent TWIZERE <v.twizere@alustudent.com>
2025-10-13 13:49:11 +0200

    latest updates

---
+++ project1/script.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+
+document.getElementById('btn').onclick = function(){
+    let radius = document.getElementById('radius').value;
+    radius = Number(radius);
+
+    let circumference = 2 * PI * radius;
+
+    document.getElementById('result').textContent = `RESULT: ${circumference}`;
+    document.getElementById('result').style.color = 'black';
+    document.getElementById('result').style.fontWeight = 'bolder';
+    
+
+};
